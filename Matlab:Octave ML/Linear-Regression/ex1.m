d2885e63f06f1bc2682612ee6d2b3d13e1fa7016
Navdeep Gill <mr.navdeepgill@gmail.com>
2015-12-07 20:21:53 -0800

    Update ex1.m
    cleanup

--- Matlab:Octave ML/Linear-Regression/ex1.m
+++ Matlab:Octave ML/Linear-Regression/ex1.m
@@ -1,10 +1,10 @@
-%%Exercise 1: Linear Regression
+%%Linear Regression
 
 %% Initialization
 clear ; close all; clc
 
 %% ==================== Part 1: Basic Function ====================
-% Complete warmUpExercise.m 
+
 fprintf('Running warmUpExercise ... \n');
 fprintf('5x5 Identity Matrix: \n');
 warmUpExercise()
